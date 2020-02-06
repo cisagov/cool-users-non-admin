3350ac9b140bfe99bdfaffea228574b6f6313326
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-06 18:35:44 -0500

    Create non-admin users and require them to have MFA to do most actions

---
+++ users.tf
@@ -0,0 +1,179 @@
+# The non-admin users being created
+resource "aws_iam_user" "users" {
+  for_each = var.users
+
+  name = each.key
+  tags = var.tags
+}
+
+# IAM policy that allows users to administer their own user
+# accounts.  This policy is pretty much copied from here:
+# https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_aws_my-sec-creds-self-manage.html
+data "aws_iam_policy_document" "iam_self_admin_doc" {
+  for_each = var.users
+
+  # Allow users to view their own account information
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:GetAccountPasswordPolicy",
+      "iam:GetAccountSummary",
+      "iam:ListVirtualMFADevices",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  # Allow users to administer their own passwords
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:ChangePassword",
+      "iam:GetUser",
+    ]
+
+    resources = [
+      aws_iam_user.users[each.key].arn,
+    ]
+  }
+
+  # Allow users to administer their own access keys
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:CreateAccessKey",
+      "iam:DeleteAccessKey",
+      "iam:ListAccessKeys",
+      "iam:UpdateAccessKey",
+    ]
+
+    resources = [
+      aws_iam_user.users[each.key].arn,
+    ]
+  }
+
+  # Allow users to administer their own signing certificates
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:DeleteSigningCertificate",
+      "iam:ListSigningCertificates",
+      "iam:UpdateSigningCertificate",
+      "iam:UploadSigningCertificate",
+    ]
+
+    resources = [
+      aws_iam_user.users[each.key].arn,
+    ]
+  }
+
+  # Allow users to administer their own ssh public keys
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:DeleteSSHPublicKey",
+      "iam:GetSSHPublicKey",
+      "iam:ListSSHPublicKeys",
+      "iam:UpdateSSHPublicKey",
+      "iam:UploadSSHPublicKey",
+    ]
+
+    resources = [
+      aws_iam_user.users[each.key].arn,
+    ]
+  }
+
+  # Allow users to administer their own git credentials
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:CreateServiceSpecificCredential",
+      "iam:DeleteServiceSpecificCredential",
+      "iam:ListServiceSpecificCredentials",
+      "iam:ResetServiceSpecificCredential",
+      "iam:UpdateServiceSpecificCredential",
+    ]
+
+    resources = [
+      aws_iam_user.users[each.key].arn,
+    ]
+  }
+
+  # Allow users to administer their own virtual MFA device
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:CreateVirtualMFADevice",
+      "iam:DeleteVirtualMFADevice",
+    ]
+
+    resources = [
+      # The MFA ARN is identical to that of the user, except that the
+      # text "user" is replaced by "mfa"
+      replace(aws_iam_user.users[each.key].arn, "user", "mfa"),
+    ]
+  }
+
+  # Allow users to administer their own (non-virtual) MFA device
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:DeactivateMFADevice",
+      "iam:EnableMFADevice",
+      "iam:ListMFADevices",
+      "iam:ResyncMFADevice",
+    ]
+
+    resources = [
+      aws_iam_user.users[each.key].arn,
+    ]
+  }
+
+  # Deny all actions but the following if no MFA device is configured
+  statement {
+    effect = "Deny"
+
+    not_actions = [
+      "iam:ChangePassword",
+      "iam:CreateVirtualMFADevice",
+      "iam:EnableMFADevice",
+      "iam:GetUser",
+      "iam:ListMFADevices",
+      "iam:ListVirtualMFADevices",
+      "iam:ResyncMFADevice",
+      "sts:GetSessionToken",
+    ]
+
+    resources = [
+      "*",
+    ]
+
+    condition {
+      test     = "BoolIfExists"
+      variable = "aws:MultiFactorAuthPresent"
+
+      values = [
+        false,
+      ]
+    }
+  }
+}
+
+# The IAM self-administration policy for our IAM users
+resource "aws_iam_user_policy" "self_managed_creds_with_mfa" {
+  for_each = var.users
+
+  name   = "SelfManagedCredsWithMFA"
+  user   = each.key
+  policy = data.aws_iam_policy_document.iam_self_admin_doc[each.key].json
+}
